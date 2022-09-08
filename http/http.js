8cfce1594d1f7186c59082db1a92f3461a81800b
Abhinav <abhinav@AVs-Mac.local>
2022-09-08 23:09:53 +0530

    [session notes]file system module - 8th sep 2022.

--- http/http.js
+++ http/http.js
@@ -1,12 +1,13 @@
 const http = require('http');
 
 const app = http.createServer((req, res) => {
-    console.log(req.url);
+    console.log(req.url); //   /req
     // console.log(res);
     console.log(req.headers.host);
     // res.write('got a connection');
     if(req.url === '/req'){
-        res.write('this is response')
+        res.write('this is response1');
+        res.write('this is response2');
     }
     if(req.url === '/') {
         res.write('<h1>Home Page</h1>');
@@ -14,9 +15,9 @@ const app = http.createServer((req, res) => {
     if(req.url === '/about') {
         res.write('<h1>About Page</h1>');
     }
-    res.end()
+    res.end('<h3>End of response</h3>')
 });
- 
+
 app.addListener('error', () => {
     console.log('an error occured');
 })
