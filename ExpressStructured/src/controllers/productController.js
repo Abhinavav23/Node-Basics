92abe1f6d561621e380f9d47dba28de0a947a518
Abhinav <abhinav@AVs-Mac.local>
2022-09-23 23:17:11 +0530

    [session notes]-EJS, res.render(), rendering- conditionals, loop, variables, functions 23rd sep 2022.

--- ExpressStructured/src/controllers/productController.js
+++ ExpressStructured/src/controllers/productController.js
@@ -1,7 +1,24 @@
 const getProduct = (req, res) => {
     // res.send('Product page')
-    // res.sendFile('/Users/abhinav/Desktop/NodeJS/ExpressStructured/src/views/product/product.html');
-    res.render('product/product')
+    // res.sendFile('/Users/abhinav/Desktop/NodeJS/ExpressStructured/src/views/product/product.htm
+    req.body
+    const data = {
+        username: 'Abhinav',
+        role: 'Instructor',
+        address: {
+            pin: 5600199
+        }
+    }
+    // fetch('https://jsonplaceholder.typicode.com/posts')
+    // .then((res) => {
+    //     return res.json()
+    // })
+    // .then((postdata) => {
+    //     res.render('product/product.ejs', {data: postdata});
+    // });
+
+    res.render('product/product.ejs', {data: [1,2,3,4,5]});
+    
 }
 
 const shirtProduct = (req, res) => {
@@ -17,3 +34,14 @@ module.exports = {
     shirtProduct,
     jeanProduct
 }
+
+
+// function calcultte(a){
+//     return a*109
+// }
+
+// const result  = calcultte(a);
+
+// console.log(result); //showing things
+let username = 'Abhinav'
+// <div>{username}</div>
